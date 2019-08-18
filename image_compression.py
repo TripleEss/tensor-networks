cc95c01b8e627f5f61ebe9a74e9787dd91444f5b
funketh <theodor.k.funke@gmail.com>
2019-08-18 19:07:08 +0200

    add experimental image_compression script

---
+++ image_compression.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import argparse
+import numpy as np
+from PIL import Image
+
+from svd import truncated_svd
+
+
+def img_to_array(path: str) -> np.ndarray:
+    img = Image.open(path)
+    return np.array(img)
+
+def compress(array: np.ndarray) -> np.ndarray:
+    u, s, v = truncated_svd(array, max(array.shape))
+    return (u @ np.diag(s) @ v).astype(array.dtype)
+
+def save_array_as_img(array: np.ndarray, path: str):
+    new_img = Image.fromarray(array)
+    new_img.save(path)
+
+def main(infile, outfile):
+    arr = img_to_array(infile)
+    compressed_arr = compress(arr)
+    save_array_as_img(compressed_arr, outfile)
+
+# %%
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+    args = parser.parse_args()
+    main(args.infile, args.outfile)
