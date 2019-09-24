64ce5fbf2ce66c32e55b0ed7c98b4c8e1ccc1fa0
funketh <theodor.k.funke@gmail.com>
2019-09-24 21:05:28 +0200

    truncated_svd pads the matrices with zeros if chi is too high & get rid of the weird robust_svd (keep it in weird_svd.py)

--- svd.py
+++ svd.py
@@ -1,43 +1,24 @@
 import logging
-import numpy as np
-import scipy.sparse.linalg
-from scipy import sparse
 from typing import Tuple
 
+import numpy as np
 
-USV = Tuple[np.ndarray, np.ndarray, np.ndarray]
 
-def sparse_svd(a: np.ndarray) -> USV:
-    new_a = np.zeros([np.shape(a)[0] + 2, np.shape(a)[1] + 2], 'float64')
-    new_a[0:np.shape(a)[0], 0:np.shape(a)[1]] = a
-    u, s, v = sparse.linalg.svds(new_a, k=min(new_a.shape[0], new_a.shape[1]) - 2)
-    u = np.fliplr(u[:-2, :])
-    s = s[::-1].real
-    v = np.flipud(v[:, :-2])
-    return u, s, v
+def truncated_svd(tensor: np.ndarray, chi: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    u, s, v = np.linalg.svd(tensor, full_matrices=False)
+    assert u.shape[1] == s.size == v.shape[0]
 
-def robust_svd(a: np.ndarray, tolerance: float = 0.0 * 1e-14) -> USV:
-    try:
-        u, s, v = np.linalg.svd(a, full_matrices=False)
-    except np.linalg.LinAlgError as e1:
-        logging.info(f'Ran into ({type(e1).__name__}: {e1})... transposing and retrying...')
-        try:
-            v, s, u = np.linalg.svd(np.transpose(a), full_matrices=False)
-            u = np.transpose(u)
-            v = np.transpose(v)
-            logging.debug('Transposing succeeded!')
-        except np.linalg.LinAlgError as e2:
-            logging.warning(f'Ran into {type(e2).__name__}: {e2}... calling sparse code...')
-            u, s, v = sparse_svd(a)
-            logging.debug('Sparse code succeeded!')
-    u = u[:, ~np.isclose(s, 0.0, atol=tolerance)]
-    s = s[~np.isclose(s, 0.0, atol=tolerance)]
-    v = v[np.transpose(~np.isclose(s, 0.0, atol=tolerance)), :]
-    return u, s, v
+    if chi <= s.size:
+        new_u = u[:, :chi]
+        new_s = s[:chi]
+        new_v = v[:chi, :]
+    else:
+        # Pad with zeros
+        new_u = np.pad(u, ((0, 0), (0, chi - u.shape[1])))
+        new_s = np.pad(s, (0, chi - s.size))
+        new_v = np.pad(v, ((0, chi - v.shape[0]), (0, 0)))
 
-def truncated_svd(tensor: np.ndarray, chi: int) -> USV:
-    u, s, v = robust_svd(tensor)
-    u = u[:, :chi]
-    s = s[:chi]
-    v = v[:chi, :]
-    return u, s, v
+    logging.debug(f'{tensor.shape} --SVD--> {u.shape} {s.shape} {v.shape}'
+                  f' --truncated/padded--> '
+                  f'{new_u.shape} {new_s.shape} {new_v.shape}')
+    return new_u, new_s, new_v
---
+++ weird_svd.py
@@ -0,0 +1,37 @@
+import logging
+from typing import Tuple
+
+import numpy as np
+from scipy.sparse.linalg import svds as scipy_sparse_svd
+
+
+def sparse_svd(a: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    new_a = np.zeros([np.shape(a)[0] + 2, np.shape(a)[1] + 2], 'float64')
+    new_a[0:np.shape(a)[0], 0:np.shape(a)[1]] = a
+    u, s, v = scipy_sparse_svd(new_a, k=min(new_a.shape[0], new_a.shape[1]) - 2)
+    u = np.fliplr(u[:-2, :])
+    s = s[::-1].real
+    v = np.flipud(v[:, :-2])
+    return u, s, v
+
+
+def robust_svd(a: np.ndarray, tolerance: float = 0.0 * 1e-14) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    try:
+        u, s, v = np.linalg.svd(a, full_matrices=False)
+    except np.linalg.LinAlgError as e1:
+        logging.warning(f'Ran into ({type(e1).__name__}: {e1})... transposing and retrying...')
+        try:
+            # How is this supposed to help?
+            v, s, u = np.linalg.svd(np.transpose(a), full_matrices=False)
+            u = np.transpose(u)
+            v = np.transpose(v)
+            logging.debug('Transposing succeeded!')
+        except np.linalg.LinAlgError as e2:
+            logging.warning(f'Ran into {type(e2).__name__}: {e2}... calling sparse code...')
+            u, s, v = sparse_svd(a)
+            logging.debug('Sparse code succeeded!')
+    # The next lines break some decompositions. What's their purpose?
+    u = u[:, ~np.isclose(s, 0.0, atol=tolerance)]
+    s = s[~np.isclose(s, 0.0, atol=tolerance)]
+    v = v[np.transpose(~np.isclose(s, 0.0, atol=tolerance)), :]
+    return u, s, v
