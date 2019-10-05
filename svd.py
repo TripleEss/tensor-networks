9525c9ab3e08a16c909d03744a67635a35b9c0d5
funketh <theodor.k.funke@gmail.com>
2019-10-05 11:05:04 +0200

    factorize truncate out of truncated_svd

--- svd.py
+++ svd.py
@@ -4,26 +4,26 @@ from typing import Tuple, Optional
 import numpy as np
 
 
-def truncated_svd(tensor: np.ndarray, chi: Optional[int]) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    if chi is not None and chi < 0:
+def truncate(u: np.ndarray, s: np.ndarray, v: np.ndarray, chi: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if chi < 0:
         raise ValueError('chi has to be at least 0')
-
-    u, s, v = np.linalg.svd(tensor, full_matrices=False)
-
-    if chi is None:
-        logging.debug(f'{tensor.shape} --SVD--> {u.shape} {s.shape} {v.shape}')
-        return u, s, v
-    elif chi <= s.size:
-        new_u = u[:, :chi]
-        new_s = s[:chi]
-        new_v = v[:chi, :]
-    else:
+    if chi > s.size:
         # Pad with zeros
-        new_u = np.pad(u, ((0, 0), (0, chi - u.shape[1])))
-        new_s = np.pad(s, (0, chi - s.size))
-        new_v = np.pad(v, ((0, chi - v.shape[0]), (0, 0)))
+        return (
+            np.pad(u, ((0, 0), (0, chi - u.shape[1]))),
+            np.pad(s, (0, chi - s.size)),
+            np.pad(v, ((0, chi - v.shape[0]), (0, 0))),
+        )
+    return u[:, :chi], s[:chi], v[:chi, :]
 
-    logging.debug(f'{tensor.shape} --SVD--> {u.shape} {s.shape} {v.shape}'
-                  f' --truncated/padded--> '
-                  f'{new_u.shape} {new_s.shape} {new_v.shape}')
-    return new_u, new_s, new_v
+
+def truncated_svd(tensor: np.ndarray, chi: Optional[int]) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    u, s, v = np.linalg.svd(tensor, full_matrices=False)
+    _log_msg = f'{tensor.shape} --SVD--> {u.shape} {s.shape} {v.shape}'
+    if chi is None:
+        logging.debug(_log_msg)
+        return u, s, v
+    u, s, v = truncate(u, s, v, chi)
+    _log_msg += f' --truncated/padded--> {u.shape} {s.shape} {v.shape}'
+    logging.debug(_log_msg)
+    return u, s, v
