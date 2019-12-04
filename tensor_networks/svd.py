dd72a16721087142f42dbc219c3739deb16bef41
funketh <45576110+funketh@users.noreply.github.com>
2019-12-04 17:24:41 +0100

    Update svd.py

--- tensor_networks/svd.py
+++ tensor_networks/svd.py
@@ -16,17 +16,11 @@ def truncated_svd(matrix: ndarray,
         pass
     elif chi < 0:
         raise ValueError('chi has to be at least 0')
-    elif chi > s.size:
-        # Pad with zeros
-        u, s, v = (
-            np.pad(u, ((0, 0), (0, chi - u.shape[1]))),
-            np.pad(s, (0, chi - s.size)),
-            np.pad(v, ((0, chi - v.shape[0]), (0, 0))),
-        )
-    else:
+    elif chi < s.size:
+        norm_s = np.linalg.norm(s)
         u, s, v = u[:, :chi], s[:chi], v[:chi, :]
         if normalize:
-            s = s * (np.linalg.norm(s) / np.linalg.norm(matrix))
+            s = s * (np.linalg.norm(s) / norm_s)
     logging.debug(_log_msg + f' --truncated/padded-->'
                              f' {u.shape} {s.shape} {v.shape}')
     return u, s, v
