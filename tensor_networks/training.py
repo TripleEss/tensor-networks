f60e94cd60e91e950720f8630d76d08518377a13
funketh <theodor.k.funke@gmail.com>
2020-04-17 18:47:09 +0200

    rearrange imports

--- tensor_networks/training.py
+++ tensor_networks/training.py
@@ -1,17 +1,16 @@
-from functools import partial
 from itertools import tee
 
 from more_itertools import consume
 
-from tensor_networks.patched_numpy import np
 from tensor_networks import utils
-from tensor_networks.utils import Direction, neutral_array
 from tensor_networks.annotations import *
 from tensor_networks.contraction import contract, tensor_product, attach
-from tensor_networks.inputs import Input
 from tensor_networks.decomposition import truncated_svd, split
+from tensor_networks.inputs import Input
+from tensor_networks.patched_numpy import np
 from tensor_networks.tensor_train import TensorTrain
 from tensor_networks.transposition import transpose_outer_indices
+from tensor_networks.utils import Direction, neutral_array
 
 
 def update(ideal: Array, output: Array, input_: Array) -> Array:
