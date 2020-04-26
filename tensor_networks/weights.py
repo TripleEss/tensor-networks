31bbe2f5d0ed64473d5aa216f8dc8cc965545207
funketh <theodor.k.funke@gmail.com>
2020-04-26 23:23:11 +0200

    make all the starting cores have a norm of 1

--- tensor_networks/weights.py
+++ tensor_networks/weights.py
@@ -7,7 +7,7 @@ from tensor_networks.utils import neutral_array
 
 def starting_weights(input_length: int, label_length: int) -> TensorTrain:
     return TensorTrain(
-        [np.stack([neutral_array(1, 2, 2)] * label_length, axis=2)]
-        + [neutral_array(2, 2, 2) for _ in range(input_length - 2)]
-        + [neutral_array(2, 2, 1)]
-    ) / sqrt(2)
+        [np.stack([neutral_array(1, 2, 2)] * label_length, axis=2) / sqrt(2 * label_length)]
+        + [neutral_array(2, 2, 2) / sqrt(2) for _ in range(input_length - 2)]
+        + [neutral_array(2, 2, 1) / sqrt(2)]
+    )
